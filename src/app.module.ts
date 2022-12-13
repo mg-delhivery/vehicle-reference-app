13f3514c220fa4b1d32cfd4c6956d7bb17ec6a1d
Derek Gordon <gordon.derek46@gmail.com>
2022-12-12 19:44:58 -0500

    add vehicles module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VehicleModule } from './vehicle/vehicle.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
-  imports: [],
+  imports: [VehicleModule, VehiclesModule],
   controllers: [AppController],
   providers: [AppService],
 })
