531b96dd910f57af26e92ba0520c7a7b8e972bb6
Vinoth Kumar Shanmugam <vinoth.shanmugam@delhivery.com>
2022-12-18 07:42:54 +0530

    add swagger doc

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Vehicles API')
+      .setVersion('1.0.0')
+      .addTag('api')
+      .build(),
+  );
+  SwaggerModule.setup('/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
